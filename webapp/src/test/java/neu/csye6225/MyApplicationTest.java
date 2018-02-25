3e5e526a8c6a43d32417cd1ba979f278a7f9b4a1
elfred012 <elfred012@gmail.com>
2018-02-24 20:56:08 -0500

    assign2 add unit test

--- webapp/src/test/java/neu/csye6225/MyApplicationTest.java
+++ webapp/src/test/java/neu/csye6225/MyApplicationTest.java
@@ -1,73 +1,135 @@
 package neu.csye6225;
 
 import neu.csye6225.controller.UserInfoController;
+import neu.csye6225.entity.UserInfo;
 import neu.csye6225.service.UserInfoService;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import static org.mockito.Mockito.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@WebAppConfiguration
+//@AutoConfigureMockMvc
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = MockServletContext.class)
 @WebAppConfiguration
-@AutoConfigureMockMvc
 public class MyApplicationTest {
 
+	private MockMvc mockMvc;
 
-	private MockMvc mvc;
-
-	@Autowired
+	@Mock
 	private UserInfoService userService;
+
+	@InjectMocks
+	private UserInfoController userController;
 
 	@Before
 	public void setUp() throws Exception {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		MockitoAnnotations.initMocks(this);
+		this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+	}
 
-		mvc = MockMvcBuilders.standaloneSetup(new UserInfoController()).setViewResolvers(viewResolver).build();
+
+	@Test
+	public void testUserInfo() {
+
+		//Create a UserInfo object which is to be tested
+		String username = "root123@163.com";
+		String password = "root1234";
+		UserInfo userInfo = new UserInfo(username, password);
+
+
+		//Create the mock object of stock service
+		UserInfo userInfoMock = mock(UserInfo.class);
+
+		// mock the behavior of stock service to return the value of various stocks
+		when(userInfoMock.getUsername()).thenReturn("root123@163.com");
+		when(userInfoMock.getPassword()).thenReturn(password);
+
 
 	}
 
 	@Test
 	public void testUserController() throws Exception {
-		RequestBuilder request = null;
 
+
+		RequestBuilder request = null;
+/*
 		request = get("/login");
-		mvc.perform(request)
+		mockMvc.perform(request)
 				.andExpect(MockMvcResultMatchers.status().isOk());
 
 		request = get("/signup");
-		mvc.perform(request)
+		mockMvc.perform(request)
 				.andExpect(MockMvcResultMatchers.status().isOk());
-
-		/*request = post("/login-check")
+*/
+		request = post("/login-check")
 				.param("id", "1")
 				.param("username", "root123@163.com")
 				.param("password", "1234567!");
-		mvc.perform(request)
+		mockMvc.perform(request)
 				.andExpect(MockMvcResultMatchers.status().isOk());
 
 		request = post("/signup-check")
 				.param("username", "root123@163.com")
 				.param("password", "1234567!");
-		mvc.perform(request)
+		mockMvc.perform(request)
 				.andExpect(MockMvcResultMatchers.status().isOk());
-*/
-
 
 	}
+
 	@Test
-	public void contextLoads() {
+	public void test_UserController_save_return_success() throws Exception {
+		//mock
+		//Mockito.doNothing().when(userService).save(Mockito.any());
+
+		//build request
+		RequestBuilder request = MockMvcRequestBuilders.get("/");
+
+		//assertion
+		mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk());
+
 	}
+
+	/*@Test
+	public void test_UserController_save_return_start_error() throws Exception {
+		//mock
+		Mockito.doThrow(new IllegalArgumentException("user error !")).when(userService).save(Mockito.any());
+
+		//build request
+		RequestBuilder request = MockMvcRequestBuilders.post("/index");
+
+		//assertion
+		mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(
+						result -> {
+							Assertions.assertThat(result.getResponse().getContentAsString()).startsWith("Welcome");
+						}
+				);
+	}*/
 
 }
