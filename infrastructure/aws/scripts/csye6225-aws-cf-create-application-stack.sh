18cb7a5044bb4cdb386f391b6af1c1c9dc2ed5bd
elfred012 <elfred012@gmail.com>
2018-02-25 13:05:04 -0500

    update for assign4

---
+++ infrastructure/aws/scripts/csye6225-aws-cf-create-application-stack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter Your Stack Name:"
+read stackname
+
+instanceName="$stackname-csye6225-instance"
+
+aws cloudformation create-stack --template-body file://./csye6225-cf-application.json --stack-name ${stackname} --parameters ParameterKey=InstanceName,ParameterValue=$instanceName
+
+aws cloudformation wait stack-create-complete --stack-name ${stackname}
+done
