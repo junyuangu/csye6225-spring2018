3629c2a8d722b8c075c04c2345d2b78136603bf3
Junyuan_Gu <gu.ju@northeastern.edu>
2018-04-14 12:32:03 -0400

    switch back to region us-east-1 and try to attach autoscaling group to deployment group

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
@@ -10,8 +10,12 @@ read stackname
 STACK_CF=""
 STACK_STATUS=""
 
+EC2_Subnet_ID=$(aws ec2 describe-subnets --filters Name=tag:aws:cloudformation:logical-id,Values=SubnetForWebServers | jq -r '.Subnets[0].SubnetId')
+EC2_Subnet2_ID=$(aws ec2 describe-subnets --filters Name=tag:aws:cloudformation:logical-id,Values=SubnetForAutoScaling | jq -r '.Subnets[0].SubnetId')
+
+
 #Create Stack:
-STACK_CF=$(aws cloudformation create-stack --template-body file://csye6225-cf-ci-cd.json --stack-name $stackname --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=CodeDeployAppName,ParameterValue=MyApplication ParameterKey=TagKey,ParameterValue=name ParameterKey=TagValue,ParameterValue=csye6225-MyAutoScalingGroup)
+STACK_CF=$(aws cloudformation create-stack --template-body file://csye6225-cf-ci-cd.json --stack-name $stackname --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=CodeDeployAppName,ParameterValue=MyApplication ParameterKey=TagKey,ParameterValue=name ParameterKey=TagValue,ParameterValue=csye6225-MyAutoScalingGroup ParameterKey=EC2SubnetId,ParameterValue=$EC2_Subnet_ID ParameterKey=EC2Subnet2Id,ParameterValue=$EC2_Subnet2_ID)
 
 #Wait until stack completely created
 echo "Please wait..."
